48bd9575e1124ad4a4a56a2ea8508fbfb2ed5096
Francisco Melicias <melicias1999@gmail.com>
2023-02-08 11:48:17 +0000

    update to test tabnet

---
+++ tabnet/test/prepareData.py
@@ -0,0 +1,160 @@
+#!pip install pytorch-tabnet wget
+from pytorch_tabnet.tab_model import TabNetClassifier
+import torch
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import SMOTE
+from sklearn.utils import shuffle
+from sklearn import metrics
+from xgboost import plot_tree
+import pandas as pd
+import numpy as np
+import os
+import wget
+from pathlib import Path
+import shutil
+import gzip
+from xgboost import XGBClassifier
+
+#sys.setrecursionlimit(1000000) 
+
+random_state=42
+np.random.seed(random_state)
+
+from matplotlib import pyplot as plt
+#%matplotlib inline
+
+df = pd.read_csv('../../data/DNN-EdgeIIoT-dataset.csv', low_memory=False)
+drop_columns = ["frame.time", "ip.src_host", "ip.dst_host", "arp.src.proto_ipv4","arp.dst.proto_ipv4", 
+         "http.file_data","http.request.full_uri","icmp.transmit_timestamp",
+         "http.request.uri.query", "tcp.options","tcp.payload","tcp.srcport",
+         "tcp.dstport", "udp.port", "mqtt.msg"]
+
+df.drop(drop_columns, axis=1, inplace=True)
+df.dropna(axis=0, how='any', inplace=True)
+df.drop_duplicates(subset=None, keep="first", inplace=True)
+df = shuffle(df)
+
+categorical_columns = []
+for col in df.columns[df.dtypes == object]:
+    if col != "Attack_type":
+        categorical_columns.append(col)
+
+featuresFromStart = [ col for col in df.columns if col not in ["Attack_label"]+["Attack_type"]]
+#print("-----Features from the start-----")
+#print(featuresFromStart)
+#print("-----Categorial features-----")
+#print(categorical_columns)
+#Display information about dataframe
+def displayInformationDataFrame(df_cop):
+    summary_df = pd.DataFrame(columns=['Data Type', 'Column Name', 'Unique Values'])
+    # Iterate through the columns of the original dataframe
+    for col in df_cop.columns:
+        # Get the data type of the column
+        dtype = df_cop[col].dtype
+        # Get the column name
+        col_name = col
+        # Get the unique values of the column
+        unique_values = df_cop[col].unique()
+        # Append a new row to the summary dataframe
+        summary_df = summary_df.append({'Data Type': dtype, 'Column Name': col_name, 'Unique Values': unique_values}, ignore_index=True)
+    # display the summary_df
+    pd.options.display.max_rows = None
+    pd.options.display.max_columns = None
+    #return display(summary_df)
+    
+displayInformationDataFrame(df)
+
+colunas_one_hot = {}
+for coluna in categorical_columns:
+    codes, uniques = pd.factorize(df[coluna].unique())
+    colunas_one_hot[coluna] = {"uniques": uniques, "codes":codes}
+    df[coluna] = df[coluna].replace(colunas_one_hot[coluna]["uniques"], colunas_one_hot[coluna]["codes"])
+    print(coluna)
+df = pd.get_dummies(data=df, columns=categorical_columns)
+displayInformationDataFrame(df)
+
+df = shuffle(df)
+n_total = len(df)
+
+features = [ col for col in df.columns if col not in ["Attack_label"]+["Attack_type"]] 
+
+le = LabelEncoder()
+le.fit(df["Attack_type"].values)
+
+train_val_indices, test_indices = train_test_split(range(n_total), test_size=0.2, random_state=random_state)
+train_indices, valid_indices = train_test_split(train_val_indices, test_size=0.25, random_state=random_state) # 0.25 x 0.8 = 0.2
+
+X_train = df[features].values[train_val_indices]
+y_train = df["Attack_type"].values[train_val_indices]
+y_train = le.transform(y_train)
+
+X_valid = df[features].values[valid_indices]
+y_valid = df["Attack_type"].values[valid_indices]
+y_valid = le.transform(y_valid)
+
+X_test = df[features].values[test_indices]
+y_test = df["Attack_type"].values[test_indices]
+y_test = le.transform(y_test)
+
+standScaler = StandardScaler()
+model_norm = standScaler.fit(X_train)
+
+X_train = model_norm.transform(X_train)
+X_test = model_norm.transform(X_test)
+X_valid = model_norm.transform(X_valid)
+
+#sm = SMOTE(random_state=random_state,n_jobs=-1)
+#X_train, y_train = sm.fit_resample(X_train, y_train)
+
+
+clf = TabNetClassifier(
+    n_d=64, n_a=64, n_steps=5,
+    gamma=1.5, n_independent=2, n_shared=2,
+    cat_idxs=[],
+    cat_dims=[],
+    cat_emb_dim=1,
+    lambda_sparse=1e-4, momentum=0.3, clip_value=2.,
+    optimizer_fn=torch.optim.Adam,
+    optimizer_params=dict(lr=2e-2),
+    scheduler_params = {"gamma": 0.95, "step_size": 20},
+    scheduler_fn=torch.optim.lr_scheduler.StepLR, epsilon=1e-15
+)
+
+max_epochs = 5 if not os.getenv("CI", False) else 2
+
+clf.fit(
+    X_train=X_train, y_train=y_train,
+    eval_set=[(X_train, y_train), (X_valid, y_valid)],
+    eval_name=['train', 'valid'],
+    max_epochs=max_epochs, patience=100,
+    batch_size=16384, virtual_batch_size=256
+)
+
+saved_filename = clf.save_model('modelTabNet')
+
+#loaded_clf = TabNetClassifier()
+#loaded_clf.load_model(saved_filename)
+
+y_pred = clf.predict(X_test)
+test_acc = accuracy_score(y_pred=y_pred, y_true=y_test)
+print(f"FINAL TEST SCORE FOR : {test_acc}")
+
+
+from sklearn import metrics
+original_labels_list = le.classes_
+fig,ax = plt.subplots(figsize=(20, 20))
+confusion_matrix = metrics.confusion_matrix(y_test, y_pred)
+cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix,display_labels= original_labels_list)
+cm_display.plot(ax=ax)
+plt.savefig("confusion_matrix.png")
+
+print(classification_report(y_test, y_pred))
